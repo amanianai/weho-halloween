41d5e91d758ff3cffde038f09381b0f50ef39977
Jonas Fleur-Aime <hello@mindofjonas.com>
2018-10-24 14:49:21 -0400

    Remove old JS code

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@ require('bootstrap');
 require('slick-carousel');
 
 $(function () {
-  var selectedClass = "";
-  $(".filter").click(function () {
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function () {
-      $("." + selectedClass).fadeIn().addClass('animation');
-      $("#gallery").fadeTo(300, 1);
-    }, 300);
-  });
-
   // Photo Gallery Slider
   var prevArrowHtml = `<div class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></div>`;
   var nextArrowHtml = `<div class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></div>`;
